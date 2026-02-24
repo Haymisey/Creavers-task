db2e7aa7d8ebd3f18e771677e0d3106ef4546ae3
Haymisey <haymanotseyoum3@gmail.com>
2026-02-24 10:27:19 +0300

    feat: added auth modules and guards for the RBAC

---
+++ frontend/src/app/services/data.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private apiUrl = 'http://localhost:5000/api';
+
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  private get headers() {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'x-auth-token': this.authService.currentUserValue.token || ''
+    });
+  }
+
+  // Admin CRUD
+  getTeachers(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/admin/teachers`, { headers: this.headers });
+  }
+
+  addTeacher(teacher: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/admin/teachers`, teacher, { headers: this.headers });
+  }
+
+  getStudents(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/admin/students`, { headers: this.headers });
+  }
+
+  addStudent(student: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/admin/students`, student, { headers: this.headers });
+  }
+
+  getSubjects(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/admin/subjects`, { headers: this.headers });
+  }
+
+  addSubject(subject: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/admin/subjects`, subject, { headers: this.headers });
+  }
+
+  getGrades(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/admin/grades`, { headers: this.headers });
+  }
+
+  addGrade(grade: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/admin/grades`, grade, { headers: this.headers });
+  }
+
+  // Teacher CRUD
+  getAssignedStudents(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/teacher/assigned-students`, { headers: this.headers });
+  }
+
+  assignMarks(markData: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/teacher/marks`, markData, { headers: this.headers });
+  }
+
+  getMarksByGrade(gradeId: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/teacher/marks/${gradeId}`, { headers: this.headers });
+  }
+
+  // Student CRUD
+  getMyMarks(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/student/marks`, { headers: this.headers });
+  }
+}

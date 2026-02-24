4eb4c245b488665ec7f6ee7d471148064461ca0b
Haymisey <haymanotseyoum3@gmail.com>
2026-02-24 10:32:02 +0300

    feat(backend-db): setup the DB with mongoDB locally

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+app.use(cors());
+
+// Define Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/admin', require('./routes/admin'));
+app.use('/api/teacher', require('./routes/teacher'));
+app.use('/api/student', require('./routes/student'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
